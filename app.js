c8492981f5cbe10b957be5a0108b1781e8bddba8
omar wael <omarwael33@gmail.com>
2022-12-01 23:04:59 +0200

    finished post method for login

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var path = require('path');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect("mongodb://localhost:27017",function(err,client){
-  if(err)throw err;
-  var db = client.db('MyDB');
-  db.collection('FirstCollection').insertOne({id:1,firstName:'tyty',lastName: 'rtrt'});
-});
+// MongoClient.connect("mongodb://localhost:27017",function(err,client){
+//   if(err)throw err;
+//  var db = client.db('MyDB');
+//   db.collection('FirstCollection').insertOne({id:1,firstName:'tyty',lastName: 'rtrt'});
+//   db.collection('FirstCollection').insertOne({id:1,userName:'omar',password: '123'});
+  
+// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +22,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',function(req,res){
-  res.render('home');
+  res.render('login');
+
+});
+app.post('/',function(req,res) {
+  var userName = req.body.username;
+  var password= req.body.password;
+  MongoClient.connect("mongodb://localhost:27017",function(err,client){
+  if(err)throw err;
+ var db = client.db('MyDB');
+ 
+   db.collection('FirstCollection').findOne({ userName: userName },function(err,result){
+  if (result!=null){
+    res.render('home');
+  }else{
+    console.log('error');
+  }
+ console.log(result);
 })
+  
+});
+
+
+});
 app.listen(3000);
 
