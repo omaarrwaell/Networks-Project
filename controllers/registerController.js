ae6115259968ab2b91392a82de5ff89413b3cdc6
AbdoRewaished <116602823+AbdoRewaished@users.noreply.github.com>
2022-12-03 20:19:43 +0200

    Done with registering into the db

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,5 +1,30 @@
-const registerView = (req, res) => {
+const userDAO = require('../models/userDao');
+
+
+getRegistrationView = (req, res) => {
     res.render("registration.ejs", {});
 }
 
-module.exports = registerView;
+registerUser = (req, res) => {
+    const {username, password} = req.body;
+    if(! username || ! password) {
+        console.log("Fill empty fields");
+        return;
+    }
+    try{
+        userDAO.findUser(username)
+        .then((user) => {
+            if(user) {
+                console.log("user already exists");
+                return;
+            }
+            userDAO.registerUser(username, password);
+            res.render("login.ejs");
+        });
+        
+    }catch(e) {
+        console.error(e);
+        res.status(500).json({error: e.message});
+    }
+}
+module.exports = {getRegistrationView, registerUser};
--- models/userDao.js
+++ models/userDao.js
@@ -1,17 +1,46 @@
-mongodb = require('mongodb');
+const mongodb = require('mongodb');
+const bcrypt = require('bcryptjs');
 
-let users; 
+let usersCollection; 
 
 module.exports = class UserDAO {
     static async injectDB(conn) {
-        if(users) {
+        if(usersCollection) {
             return;
         }
         try {
-            users = await conn.db("users").collection("details");
+            usersCollection = await conn.db("users").collection("details");
             console.log("Connected to Database..");
         }catch(e) {
             console.error("Unable to establish collection handles in userDAO: " + e);
         }
     }
+
+    static async registerUser(username, password) {
+        try{
+            const newUser = {
+                username: username,
+                saved_places: []
+            }
+            // Hashing Password
+            const saltRounds = 10;
+            bcrypt.hash(password, saltRounds, (err, hash) => {
+                if(err)
+                    throw err;
+                newUser.password = hash;
+                usersCollection.insertOne(newUser);
+            })
+        }catch(e) {
+            console.error("Unable to post user");
+            return {error: e};
+        }
+    }
+    static async findUser(username) {
+        try {
+            return usersCollection.findOne({username: username});
+        }catch(e) {
+            console.error(e);
+        }
+    }
 }
+
