bb4ae6a5d4cb02db2f39a97a56934e5568fb0786
AbdoRewaished <116602823+AbdoRewaished@users.noreply.github.com>
2022-12-06 18:57:38 +0200

    Some code formatting

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,47 +1,36 @@
 const userDAO = require('../models/userDao');
-var emptyError = "Please fill in all fields";
-var passError= "The password you entered is in correct";
-var UsernameError =  "Username is incorrect";
+const emptyError = "Please fill in all fields";
+const passError= "The password you entered is in correct";
+const UsernameError =  "Username is incorrect";
 const bcrypt = require('bcryptjs');
+
+
 const loginView = (req, res) => {
-    res.render('login.ejs', { emptyError: "" ,passError:"",UsernameError:""});
+    res.render('login.ejs', { emptyError: "", passError: "", UsernameError: ""});
 }
 
 logUserIn = (req, res) => {
-    var userName = req.body.username;
-    var password = req.body.password;
+    const username = req.body.username;
+    const password = req.body.password;
 
-    if (! userName  ||  ! password ) {
-       
-        res.render('login', { emptyError: emptyError ,passError:"",UsernameError:""});
-    }else{
-    userDAO.findUser(userName).then((user) => {
-     if(user){
-        var passMatch = bcrypt.compareSync(password, user.password);
-      
-        
-            if(passMatch){
-
-            res.render('home');
-            return;
-            }
-            else{
-                res.render('login',{passError: passError, emptyError: "",UsernameError:""});
-            }
-        }else{
-            res.render('login',{UsernameError:UsernameError,emptyError: "" ,passError:""});
-        }
-
-        },
-            
-        
-       
-        
-    )
-       
-    
+    if (! username  ||  ! password ) {
+        res.render('login', { emptyError: emptyError, passError: "", UsernameError: ""});
+        return;
     }
     
+    userDAO.findUser(username).then((user) => {
+        if(! user) {
+            res.render('login', {UsernameError: UsernameError, emptyError: "", passError: ""});
+            return;
+        }
+        let passMatch = bcrypt.compareSync(password, user.password);    
+        if(passMatch){
+            res.render('home');
+            return;
+        }
+        res.render('login', {passError: passError, emptyError: "", UsernameError: ""});
+    })
+       
 }
 
 
--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const login = require('../controllers/loginController');
 
 router.get('/', login.loginView);
-router.post('/',login.logUserIn);
+router.post('/', login.logUserIn);
 
 
 module.exports = router;
