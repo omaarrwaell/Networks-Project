1fb48936cee0d892cef58ffcadfdf6466bbacf33
omar wael <omarwael33@gmail.com>
2022-12-06 17:50:55 +0200

    login finished except session

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,38 +1,48 @@
 const userDAO = require('../models/userDao');
 var emptyError = "Please fill in all fields";
+var passError= "The password you entered is in correct";
+var UsernameError =  "Username is incorrect";
 const bcrypt = require('bcryptjs');
 const loginView = (req, res) => {
-    res.render('login.ejs', { emptyError: "" });
+    res.render('login.ejs', { emptyError: "" ,passError:"",UsernameError:""});
 }
 
 logUserIn = (req, res) => {
     var userName = req.body.username;
     var password = req.body.password;
 
-    if (userName == null || password == null) {
-        res.render('login', { emptyError: emptyError });
+    if (! userName  ||  ! password ) {
+       
+        res.render('login', { emptyError: emptyError ,passError:"",UsernameError:""});
     }else{
     userDAO.findUser(userName).then((user) => {
-     
+     if(user){
         var passMatch = bcrypt.compareSync(password, user.password);
       
-        if (user) {
+        
             if(passMatch){
 
             res.render('home');
             return;
             }
             else{
-                res.render('login',{passError: "The password you entered is in correct "});
+                res.render('login',{passError: passError, emptyError: "",UsernameError:""});
             }
-
         }else{
-            res.render('login',{UsernameError: "Username is incorrect"});
+            res.render('login',{UsernameError:UsernameError,emptyError: "" ,passError:""});
         }
-       
 
-    })
+        },
+            
+        
+       
+        
+    )
+       
+    
+    }
+    
 }
-}
+
 
 module.exports = { loginView, logUserIn };
