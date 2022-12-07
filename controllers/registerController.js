c91343ce04726b0fd0d1352afe15f8705b5bfc73
AbdoRewaished <116602823+AbdoRewaished@users.noreply.github.com>
2022-12-07 16:51:32 +0200

    saved

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,23 +1,28 @@
 const userDAO = require('../models/userDao');
-
+const empty_field_error = "fill empty field";
+const user_already_exists ="Username Already Exists. Please Log in";
 
 getRegistrationView = (req, res) => {
-    res.render("registration.ejs", {});
+    res.render("registration.ejs", {usernameError: "", passwordError: ""});
 }
 
 registerUser = (req, res) => {
     const {username, password} = req.body;
     if(! username) {
-        res.render('registration.ejs', {usernameError: "fill empty field"})
+        res.render('registration', {usernameError: empty_field_error, passwordError: ""})
     }
     if(! password) {
-        res.render('registration.ejs', {passwordError: "fill empty field"})
+        res.render('registration', {usernameError: "", passwordError: empty_field_error});
+        return;
     }
     try{
         userDAO.findUser(username)
         .then((user) => {
+            
+
             if(user) {
-                res.render('registration.ejs', {usernameError: "Username Already Exists. Please Log in"})
+                res.render('registration', {usernameError: user_already_exists, passwordError: ""});
+                return;
             }
             userDAO.registerUser(username, password);
             res.render("login.ejs");
