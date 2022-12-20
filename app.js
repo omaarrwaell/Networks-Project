496cdb4a20e548ad4e117b61c066832d1a160767
ashraf <mohamedxman859@gmail.com>
2022-12-20 16:43:33 +0200

    appjs

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.use('/', require(routes_folder + "cities.js"));
 app.use('/', require(routes_folder + "home.js"));
 app.use('/', require(routes_folder + "islands.js"));
 app.use('/', require(routes_folder + "hiking.js"));
+app.use('/', require(routes_folder + "search.js"));
+app.use('/', require(routes_folder + "wanttogo.js"));
 
 port = process.env.port || 3000;
 
